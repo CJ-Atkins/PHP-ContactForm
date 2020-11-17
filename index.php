585c48ea40d5bf0300ae3376cd734a4a48e189f6
Connor Atkins <connoratkins@hotmail.co.uk>
2020-11-17 18:20:25 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Contact US</title>
+</head>
+<body>
+     <nav class="navbar navbar-default">
+          <div class="container">
+          <div class="navbar-header">
+          <a href="index.php" class="navbar-brand">My Website</a>
+          </div>
+          </div>
+     </nav>
+</body>
+</html>
